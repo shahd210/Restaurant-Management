e4a94ddb36586dc85196d39f6a531acba0d2ebbf
shahd khairy <shahdkhairy2026@gmail.com>
2026-06-10 14:43:32 +0300

    login

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,6 +1,7 @@
 import {Routes ,Route} from "react-router-dom"
 import './App.css'
 import Home from './Pages/Home'
+import Register from "./Components/Dashboard/Register/Register"
 import Login from "./Components/Dashboard/Login/Login"
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
   <Route path="/" element={<Home/>} />
   <Route path="/menu" element={<h1>menu</h1>}  />
   <Route path="/card" element={<h1>card</h1>}  />
-  <Route path="/login" element={<Login/>} />
+  <Route path="/register" element={<Register/>} />
+  <Route path="/login" element={<Login/>}/>
   <Route path="/notification" element={<h1>notification</h1>}  />
      </Routes>
     </>
--- Front/src/Components/Dashboard/Login/Login.jsx
+++ Front/src/Components/Dashboard/Login/Login.jsx
@@ -1,50 +1,16 @@
 import styles from "./Login.module.css"
-import login from "../../../assets/login.jpg"
- function Login(){
-
+import Loginimg from "../../../assets/Login.png"
+function Login (){
     return(
         <>
-        <div className={` d-flex justify-content-between  `}>
-            
-            <div className="col-5" >
-                <img className={` ${styles.customimg}`} src={login} alt="" />
+        <div className={`d-flex justify-content-between `}>
+            <div className={`${styles.customimg} col-5`}>
+           <img src={Loginimg}  />
             </div>
-             <div className={`${styles.Loginpg}  col-7`}>
-                <div>
-                      <h3 className="fw-semibold">Aurora Dining</h3>
-       <h4 className="fw-semibold">Create Your Account</h4>
-       <p>Start your journey into the world of elite gastronomy.</p>
-                </div>
-      <div className="mt-5 border-top pt-5">
-      <div className={`form-group mb-3  `}>
-        <label className="fw-semibold" htmlFor="">Full Name</label><br />
-        <input  className={`${styles.custominp} px-2 py-3`}  type="text" placeholder="Shahd Khairy" />
-      </div>
-
-      <div className={`form-group mb-3  `}>
-        <label className="fw-semibold" htmlFor="">Email Address</label><br />
-        <input  className={`${styles.custominp} px-2 py-3`}  type="email" placeholder="Shahd@gmail.com" />
-      </div>
-
-      <div className={`form-group mb-3   `}>
-        <label className="fw-semibold" htmlFor="">Password</label><br />
-        <input  className={`${styles.custominp} px-2 py-3`}  type="password" placeholder="******" />
-      </div>
-
-      <div className={`form-group mb-5   `}>
-        <label className={`${styles.customlabel} fw-semibold`} htmlFor="">Confirm Password</label><br />
-        <input  className={`${styles.custominp} px-2 py-3`}  type="password" placeholder="******" />
-      </div>
-      </div>
-      <div>
-        <button className={`btn text-white py-3 fw-bold shadow rounded-3 ${styles.custombtn}`}>Create Account</button>
-      </div>
-             </div>
-        
-           
-
+            <div className={`${styles} col-7`}> 
+ujujh
+            </div>
         </div>
-        
         </>
     )
 }
